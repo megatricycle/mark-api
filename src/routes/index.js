f5835d09c1835469950f8dff35dcec0303daa1f8
Peter Rupa <megatricycle@gmail.com>
2017-05-21 17:52:09 +0800

    Disabled authentication for dev purposes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import userRoutes from './users';
 import sessionRoutes from './session';
 import productRoutes from './products';
-import { forbidden } from '../constants/errorTypes';
+// import { forbidden } from '../constants/errorTypes';
 
 router.get('/', (req, res) => {
     res.send({
@@ -15,13 +15,15 @@ router.get('/', (req, res) => {
 router.use('/session', sessionRoutes);
 
 // all routes beyond this must have a user logged in
-router.use((req, res, next) => {
-    if (!req.user) {
-        next(forbidden);
-    } else {
-        next();
-    }
-});
+
+// @TODO: authenticate log in
+// router.use((req, res, next) => {
+//     if (!req.user) {
+//         next(forbidden);
+//     } else {
+//         next();
+//     }
+// });
 
 router.use('/users', userRoutes);
 router.use('/products', productRoutes);
